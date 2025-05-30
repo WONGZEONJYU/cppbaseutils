4d6bd1cb73d1463f9bd89e871d9f3e1f11cc9e12
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2025-05-30 19:19:35 +0800

    modified

---
+++ XThread/xthread.hpp
@@ -0,0 +1,17 @@
+#ifndef XTHREAD_HPP
+#define XTHREAD_HPP
+
+#include "../XHelper/xhelper.hpp"
+
+class XThread {
+    X_DISABLE_COPY_MOVE(XThread)
+protected:
+    explicit XThread() = default;
+
+public:
+
+
+
+};
+
+#endif
