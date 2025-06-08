64d5f796f59fd7ce88e179a97f7e9048c14bffaf
fuck_if <wongjy13005151320@outlook.com>
2025-06-09 01:06:43 +0800

    m

--- Test/main.cpp
+++ Test/main.cpp
@@ -2,56 +2,11 @@
 #include <iostream>
 #include <XThread/xabstractthread.hpp>
 
-class A : public xtd::XAbstractThread{
-    void Main() override{
-        std::cout << __PRETTY_FUNCTION__ << " Begin!\n";
-        while (!is_exit()){
-            std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        }
-        std::cout << __PRETTY_FUNCTION__ << " End!\n";
-    }
-
-    void doWork(std::any &arg) override{
-
-    }
-
-public:
-    A() = default;
-};
-
-class B:public xtd::XAbstractThread{
-    void Main() override{
-        std::cout << __PRETTY_FUNCTION__ << " Begin!\n";
-        std::cout << __PRETTY_FUNCTION__ << "End!\n";
-    }
-public:
-    B() = default;
-};
-
 using namespace std;
-
-void f(std::any data_){
-    std::any_cast<string&>(data_) = "0123456789";
-}
 
 int main(const int argc,const char **const argv){
     (void )argc,(void )argv;
 
-    std::string s("123");
-    f(s);
-    std::cout << "s = " << s << std::endl;
-
-    A a;
-    a.start();
-
-    {
-        B b;
-        a.set_next(&b);
-        b.start();
-        std::this_thread::sleep_for(10ms);
-    }
-
-    std::this_thread::sleep_for(10ms);
 
     return 0;
 }
