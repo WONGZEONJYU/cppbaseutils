c37bf43ad3deb26a332b64cdb1b4d20401e45f9d
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2025-06-09 20:57:14 +0800

    m

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,9 +1,10 @@
 #include "main.hpp"
 #include <iostream>
 #include <XThread/xabstractthread.hpp>
+#include <semaphore>
+#include <condition_variable>
 
 using namespace std;
-
 
 int main(const int argc,const char **const argv){
     (void )argc,(void )argv;
