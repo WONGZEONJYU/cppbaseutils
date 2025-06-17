20bad4f41efc48503f6cb9ab0eba910497917024
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2025-06-17 20:55:01 +0800

    m

--- Test/main.cpp
+++ Test/main.cpp
@@ -23,7 +23,15 @@ int main(const int argc,const char **const argv){
     for (int i{};i < 20;++i){
        std::make_shared<A>(i)->joinThreadPool(pool2);
     }
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    //std::this_thread::sleep_for(std::chrono::seconds(10));
     std::make_shared<A>(20)->joinThreadPool(pool2);
+    int ar = 21;
+    const auto r = pool2->tempTaskJoin([](const int &id){
+        for (int i {}; i < 3;++i){
+            std::cout << __PRETTY_FUNCTION__ << " id = " << id << "\n";
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    },std::ref(ar));
+
     return 0;
 }
