4b6122f9f30105934d7df88bbf67862e53460bdd
fuck_if <wongjy13005151320@outlook.com>
2025-06-20 02:39:48 +0800

    1.返回值等待使用原子bool等待替换信号量等待(发现一个新bug，就是获取返回值同时调用多次会被阻塞,是重新被等待函数阻塞,待修复!)

--- XThreadPool/xabstracttask2.cpp
+++ XThreadPool/xabstracttask2.cpp
@@ -1,8 +1,15 @@
 #include "xabstracttask2.hpp"
-#include <semaphore>
+#include "xthreadpool2.hpp"
 #include <future>
 #include <iostream>
-#include "xthreadpool2.hpp"
+
+#define USE_ATOMIC 1
+
+#if USE_ATOMIC
+#include <XAtomic/xatomic.hpp>
+#else
+#include <semaphore>
+#endif
 
 XTD_NAMESPACE_BEGIN
 XTD_INLINE_NAMESPACE_BEGIN(v1)
@@ -10,17 +17,22 @@ XTD_INLINE_NAMESPACE_BEGIN(v1)
 class XAbstractTask2::XAbstractTask2Private final {
     enum class Private{};
 public:
+    static inline thread_local bool sm_isWorkThread{};
+#if USE_ATOMIC
+    XAtomicBool m_allow{};
+#else
+    std::binary_semaphore m_allow{0};
+#endif
+    std::promise<std::any> m_result{};
+    std::weak_ptr<XAbstractTask2> m_next{};
+    std::function<bool()> m_is_running{};
+
     explicit XAbstractTask2Private(Private){}
     ~XAbstractTask2Private() = default;
 
     static auto create(){
         return std::make_shared<XAbstractTask2Private>(Private{});
     }
-
-    std::binary_semaphore m_semaphore{0};
-    std::promise<std::any> m_result{};
-    std::weak_ptr<XAbstractTask2> m_next{};
-    std::function<bool()> m_is_running{};
 };
 
 XAbstractTask2::XAbstractTask2():
@@ -32,23 +44,46 @@ bool XAbstractTask2::is_running_() const {
 
 void XAbstractTask2::operator()() {
     try {
+#if 0
+        m_d_->sm_isWorkThread = true;
         set_result_(run());
+        m_d_->sm_isWorkThread = false;
+#else
+        set_result_(run());
+#endif
     } catch (const std::exception &e) {
         std::cerr << __PRETTY_FUNCTION__ << " exception msg : " << e.what() << "\n";
     }
 }
 
-std::any XAbstractTask2::result_() const{
+std::any XAbstractTask2::result_() const {
+#if 0
+    if (m_d_->sm_isWorkThread){
+        std::cerr << __PRETTY_FUNCTION__ << " tips: WorkThread call invalid\n" << std::flush;
+        return {};
+    }
+#endif
+
     std::cout << __PRETTY_FUNCTION__ << " begin\n" << std::flush;
-    m_d_->m_semaphore.acquire();
+#if USE_ATOMIC
+    m_d_->m_allow.m_x_value.wait({},std::memory_order_acquire);
+    m_d_->m_allow.storeRelease({});
+#else
+    m_d_->m_allow.acquire();
+#endif
     std::cout << __PRETTY_FUNCTION__ << " end\n" << std::flush;
     return m_d_->m_result.get_future().get();
 }
 
-void XAbstractTask2::set_result_(const std::any &v) const{
-    m_d_->m_result= {};
+void XAbstractTask2::set_result_(const std::any &v) const {
+    m_d_->m_result = {};
     m_d_->m_result.set_value(v);
-    m_d_->m_semaphore.release();
+#if USE_ATOMIC
+    m_d_->m_allow.storeRelease(true);
+    m_d_->m_allow.m_x_value.notify_all();
+#else
+    m_d_->m_allow.release();
+#endif
 }
 
 void XAbstractTask2::set_exit_function_(std::function<bool()> &&f) const {
