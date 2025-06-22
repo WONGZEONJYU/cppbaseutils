c5c1752a8b3c0b9f595c5179f33c2f72579b2b13
fuck_if <wongjy13005151320@outlook.com>
2025-06-22 22:34:49 +0800

    版本头文件

---
+++ XHelper/xversion.hpp
@@ -0,0 +1,11 @@
+#ifndef X_NAMESPACE_HPP
+#define X_NAMESPACE_HPP 1
+
+#define XTD_VERSION "0.0.1"
+#define XTD_NAMESPACE_BEGIN namespace xtd {
+#define XTD_NAMESPACE_END }
+
+#define XTD_INLINE_NAMESPACE_BEGIN(name) inline namespace name {
+#define XTD_INLINE_NAMESPACE_END XTD_NAMESPACE_END
+
+#endif
