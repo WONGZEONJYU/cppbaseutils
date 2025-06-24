f0f263675357a24a7191b564e4e16db3ae09d955
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2025-06-24 22:46:36 +0800

    增设拥有者,只有在执行完才会释放

--- XThreadPool/xthreadpool2.hpp
+++ XThreadPool/xthreadpool2.hpp
@@ -2,6 +2,7 @@
 #define X_THREADPOOL2_HPP
 
 #include <XThreadPool/xabstracttask2.hpp>
+#include <XHelper/xtypetraits.hpp>
 
 XTD_NAMESPACE_BEGIN
 XTD_INLINE_NAMESPACE_BEGIN(v1)
@@ -18,11 +19,17 @@ class XThreadPool2;
 using XThreadPool2_Ptr = std::shared_ptr<XThreadPool2>;
 
 class XThreadPool2 final : public std::enable_shared_from_this<XThreadPool2> {
-
     class XThreadPool2Private;
-    using XThreadPool2Private_Ptr = std::unique_ptr<XThreadPool2Private>;
-    mutable  XThreadPool2Private_Ptr m_d_{};
-    X_DECLARE_PRIVATE_D(m_d_,XThreadPool2Private)
+    class XThreadPool2Data {
+        X_DISABLE_COPY_MOVE(XThreadPool2Data)
+    protected:
+        XThreadPool2Data() = default;
+    public:
+        virtual ~XThreadPool2Data() = default;
+    };
+    using XThreadPool2Data_Ptr = std::unique_ptr<XThreadPool2Data>;
+    mutable XThreadPool2Data_Ptr m_d_ptr_{};
+    X_DECLARE_PRIVATE(XThreadPool2)
 
     class XBaseTemporaryTasks {
     protected:
@@ -38,23 +45,24 @@ class XThreadPool2 final : public std::enable_shared_from_this<XThreadPool2> {
         using decayed_Tuple_ = std::tuple<std::decay_t<Fn>,std::decay_t<Args>...>;
         mutable decayed_Tuple_ m_tuple_{};
 
-        std::any run() override {
+        std::any run() const override {
             using indices = std::make_index_sequence<std::tuple_size_v<decayed_Tuple_>>;
+            const auto& call{*this};
             if constexpr (std::is_same_v<ReturnType,void>){
-                (*this)(indices{});
+                call(indices{});
                 return {};
             }else{
-                return (*this)(indices{});
+                return call(indices{});
             }
         }
 
         template<size_t ...I>
-        ReturnType operator()(std::index_sequence<I...>) {
+        ReturnType operator()(std::index_sequence<I...>) const{
             return std::invoke(std::get<I>(std::forward<decayed_Tuple_>(m_tuple_))...);
         }
 
     public:
-        explicit XTemporaryTasksImpl(Private,Fn && fn,Args && ...args):
+        explicit XTemporaryTasksImpl(Private,Fn && fn,Args && ...args):XAbstractTask2(CONST_RUN),
         m_tuple_{std::forward<std::decay_t<Fn>>(fn),std::forward<std::decay_t<Args>>(args)...}{}
         ~XTemporaryTasksImpl() override = default;
     };
@@ -94,7 +102,7 @@ public:
     [[maybe_unused]] void start(const XSize_t &threadSize = cpuThreadsCount());
 
     ///停止线程池,线程池内部的线程调用无效
-    void stop();
+    void stop() const;
 
     /// 检查线程池是否运行
     /// @return ture or false
@@ -126,21 +134,21 @@ public:
 
     /// 模式设置,线程池启动后设置无效
     /// @param mode
-    [[maybe_unused]] void setMode(const Mode &mode);
+    [[maybe_unused]] void setMode(const Mode &mode) const;
 
     /// @return 获取当前线程池模式
     [[maybe_unused]][[nodiscard]] Mode getMode() const;
 
     /// 线程数阈值设置,线程池启动后设置无效
     /// @param num
-    [[maybe_unused]] void setThreadsSizeThreshold(const XSize_t &num);
+    [[maybe_unused]] void setThreadsSizeThreshold(const XSize_t &num) const;
 
     /// @return 线程池线程数量阈值
     [[maybe_unused]] [[nodiscard]] XSize_t getThreadsSizeThreshold() const;
 
     /// 任务数阈值设置,线程池启动后设置无效
     /// @param num
-    [[maybe_unused]] void setTasksSizeThreshold(const XSize_t &num);
+    [[maybe_unused]] void setTasksSizeThreshold(const XSize_t &num) const;
 
     /// @return 线程池任务数量阈值
     [[maybe_unused]] [[nodiscard]] XSize_t getTasksSizeThreshold() const;
@@ -168,7 +176,7 @@ public:
     /// @return 线程池对象
     [[maybe_unused]] [[nodiscard]] static XThreadPool2_Ptr create(const Mode &mode = FixedModel);
 
-    explicit XThreadPool2(const Mode &,XThreadPool2Private_Ptr);
+    explicit XThreadPool2(const Mode &,XThreadPool2Data_Ptr &&);
 
     ~XThreadPool2();
 
