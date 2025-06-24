7884686c7a6617657d73d51f871f2ee042e35449
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2025-06-24 21:33:16 +0800

    m

--- XHelper/xhelper.hpp
+++ XHelper/xhelper.hpp
@@ -4,41 +4,53 @@
 #include <XHelper/xversion.hpp>
 #include <string_view>
 #include <utility>
-#include <XHelper/xtypetraits.hpp>
 
-#define X_DISABLE_COPY(Class) \
-    Class (const Class &) = delete;\
-    Class &operator=(const Class &) = delete;
+#define X_DISABLE_COPY(...) \
+    __VA_ARGS__ (const __VA_ARGS__ &) = delete; \
+    __VA_ARGS__ &operator=(const __VA_ARGS__ &) = delete;
 
-#define X_DISABLE_COPY_MOVE(Class) \
-    X_DISABLE_COPY(Class) \
-    Class (Class &&) = delete; \
-    Class &operator=(Class &&) = delete;
+#define X_DISABLE_COPY_MOVE(...) \
+    X_DISABLE_COPY(__VA_ARGS__) \
+    __VA_ARGS__ (__VA_ARGS__ &&) = delete; \
+    __VA_ARGS__ &operator=(__VA_ARGS__ &&) = delete;
 
-#define X_DEFAULT_COPY(Class) \
-    Class (const Class &) = default;\
-    Class &operator=(const Class &) = default;
+#define X_DEFAULT_COPY(...) \
+    __VA_ARGS__ (const __VA_ARGS__ &) = default;\
+    __VA_ARGS__ &operator=(const __VA_ARGS__ &) = default;
 
-#define X_DEFAULT_MOVE(Class)\
-    Class (Class &&) = default;\
-    Class &operator=(Class &&) = default;
+#define X_DEFAULT_MOVE(...)\
+    __VA_ARGS__ (__VA_ARGS__ &&) = default; \
+    __VA_ARGS__ &operator=(__VA_ARGS__ &&) = default;
 
-#define X_DEFAULT_COPY_MOVE(Class) \
-    X_DEFAULT_COPY(Class) \
-    X_DEFAULT_MOVE(Class)\
+#define X_DEFAULT_COPY_MOVE(...) \
+    X_DEFAULT_COPY(__VA_ARGS__)\
+    X_DEFAULT_MOVE(__VA_ARGS__)
 
 template <typename T> inline T *xGetPtrHelper(T *ptr) noexcept { return ptr; }
 template <typename Ptr> inline auto xGetPtrHelper(Ptr &ptr) noexcept -> decltype(ptr.get())
 { static_assert(noexcept(ptr.get()), "Smart d pointers for X_DECLARE_PRIVATE must have noexcept get()"); return ptr.get(); }
 
-#define X_DECLARE_PRIVATE_D(D_ptr, Class) \
-    inline Class* d_func() noexcept \
-    { return reinterpret_cast<Class*>(xGetPtrHelper(D_ptr)); } \
-    inline const Class* d_func() const noexcept \
-    { return reinterpret_cast<const Class *>(xGetPtrHelper(D_ptr)); } \
-    friend class Class;
+#define X_DECLARE_PRIVATE(Class) \
+    inline Class##Private* d_func() noexcept \
+    { return reinterpret_cast<Class##Private*>(xGetPtrHelper(m_d_ptr_)); } \
+    inline const Class##Private* d_func() const noexcept \
+    { return reinterpret_cast<const Class##Private *>(xGetPtrHelper(m_d_ptr_)); } \
+    friend class Class##Private;
 
-#define X_D(Class) auto * const d{d_func()}
+#define X_DECLARE_PRIVATE_D(D_ptr, Class) \
+    inline Class##Private* d_func() noexcept \
+    { return reinterpret_cast<Class##Private*>(xGetPtrHelper(D_ptr)); } \
+    inline const Class##Private* d_func() const noexcept \
+    { return reinterpret_cast<const Class##Private *>(xGetPtrHelper(D_ptr)); } \
+    friend class Class##Private;
+
+#define X_DECLARE_PUBLIC(...) \
+    inline __VA_ARGS__ * x_func() noexcept { return static_cast<__VA_ARGS__ *>(m_x_ptr_); } \
+    inline const __VA_ARGS__ * x_func() const noexcept { return static_cast<const __VA_ARGS__ *>(m_x_ptr_); } \
+    friend class __VA_ARGS__;
+
+#define X_D(Class) Class##Private * const d{d_func()}
+#define X_X(__VA_ARGS__) __VA_ARGS__ * const x{x_func()}
 
 #define X_ASSERT(cond) ((cond) ? static_cast<void>(0) : xtd::x_assert(#cond, __FILE__, __LINE__))
 #define X_ASSERT_W(cond, where, what) ((cond) ? static_cast<void>(0) : xtd::x_assert_what(where, what, __FILE__, __LINE__))
