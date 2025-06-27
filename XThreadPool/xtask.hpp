2740d9ffa4662a3ce867beab01da2f476c691d83
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2025-06-27 15:22:27 +0800

    改名

--- XThreadPool/xtask.hpp
+++ XThreadPool/xtask.hpp
@@ -6,31 +6,31 @@
 XTD_NAMESPACE_BEGIN
 XTD_INLINE_NAMESPACE_BEGIN(v1)
 
-template<typename ...>class XTask2{};
+template<typename ...>class XTask;
 
 /**
  * 开发者继承此类去重写run函数
- * XTask2<> run函数不带const
- * XTask2<const void> 带const
+ * XTask2<NonConst> run函数不带const
+ * XTask2<Const> 带const
  */
 enum class NonConst{};
 template<>
-class [[maybe_unused]] XTask2<NonConst> : public XAbstractTask {
+class [[maybe_unused]] XTask<NonConst> : public XAbstractTask {
     std::any run() override = 0;
 protected:
-    explicit XTask2():XAbstractTask(NON_CONST_RUN){}
+    explicit XTask():XAbstractTask(NON_CONST_RUN){}
 public:
-    ~XTask2() override = default;
+    ~XTask() override = default;
 };
 
 enum class Const{};
 template<>
-class [[maybe_unused]] XTask2<Const> : public XAbstractTask {
+class [[maybe_unused]] XTask<Const> : public XAbstractTask {
     std::any run() const override = 0;
 protected:
-    explicit XTask2() : XAbstractTask(CONST_RUN){}
+    explicit XTask() : XAbstractTask(CONST_RUN){}
 public:
-    ~XTask2() override = default;
+    ~XTask() override = default;
 };
 
 XTD_INLINE_NAMESPACE_END
