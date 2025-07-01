c68bae679b5d365265f6ddb25bc18613f9f25900
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2025-07-01 13:37:33 +0800

    小修改

--- XHelper/xhelper.cpp
+++ XHelper/xhelper.cpp
@@ -1,47 +1,53 @@
 #include "xhelper.hpp"
 #include <iostream>
 
-void xtd::v1::x_assert(const char *expr, const char *file,const int &line) noexcept{
+XTD_NAMESPACE_BEGIN
+XTD_INLINE_NAMESPACE_BEGIN(v1)
+
+void x_assert(const char *expr, const char *file,const int &line) noexcept{
     std::cerr << "ASSERT:" << expr <<
         " in file " << file <<
-            ", line " << line << "\n";
+            ", line " << line << "\n" << std::flush;
 }
 
-void xtd::v1::x_assert(const std::string &expr, const std::string & file,const int &line) noexcept{
+void x_assert(const std::string &expr, const std::string & file,const int &line) noexcept{
     std::cerr << "ASSERT: " << expr <<
         " in file " << file <<
-            ", line " << line << "\n";
+            ", line " << line << "\n" << std::flush;
 }
 
-void xtd::v1::x_assert(const std::string_view& expr, const std::string_view& file, const int& line) noexcept{
+void x_assert(const std::string_view& expr, const std::string_view& file, const int& line) noexcept{
     std::cerr << "ASSERT: " << expr <<
         " in file " << file <<
-            ", line " << line << "\n";
+            ", line " << line << "\n" << std::flush;
 }
 
-void xtd::v1::x_assert_what(const char* where, const char* what,
+void x_assert_what(const char* where, const char* what,
     const char* file,const int &line) noexcept{
     std::cerr <<
         "ASSERT failure in " <<where <<
             ", what:" << what <<
                 " in file " << file <<
-                    ", line " << line << "\n";
+                    ", line " << line << "\n" << std::flush;
 }
 
-void xtd::v1::x_assert_what(const std::string& where, const std::string& what,
+void x_assert_what(const std::string& where, const std::string& what,
     const std::string& file,const int& line) noexcept{
     std::cerr <<
     "ASSERT failure in " <<where <<
         ", what:" << what <<
             " in file " << file <<
-                ", line " << line << "\n";
+                ", line " << line << "\n" << std::flush;
 }
 
-void xtd::v1::x_assert_what(const std::string_view &where, const std::string_view &what,
+void x_assert_what(const std::string_view &where, const std::string_view &what,
     const std::string_view &file,const int &line) noexcept{
     std::cerr <<
     "ASSERT failure in " << where <<
         ", what:" << what <<
             " in file " << file <<
-                ", line " << line << "\n";
+                ", line " << line << "\n" << std::flush;
 }
+
+XTD_INLINE_NAMESPACE_END
+XTD_NAMESPACE_END
