5f9ea9975dd56b342056c886da5145cf768249b3
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2025-07-07 21:41:12 +0800

    修复错误

--- XHelper/xhelper.hpp
+++ XHelper/xhelper.hpp
@@ -129,7 +129,7 @@ void x_assert_what(const std::string_view &, const std::string_view &what,
     const std::string_view &file,const int &line) noexcept;
 
 template<typename T, typename ... Args>
-static auto make_Unique(Args && args) noexcept -> std::unique_ptr<T> {
+static auto make_Unique(Args && ...args) noexcept -> std::unique_ptr<T> {
     try{
         return std::make_unique<T>(std::forward<Args>(args)...);
     }catch (const std::exception &){
@@ -138,7 +138,7 @@ static auto make_Unique(Args && args) noexcept -> std::unique_ptr<T> {
 }
 
 template<typename T, typename ... Args>
-static auto make_Shared(Args && args) noexcept -> std::shared_ptr<T> {
+static auto make_Shared(Args && ...args) noexcept -> std::shared_ptr<T> {
     try{
         return std::make_shared<T>(std::forward<Args>(args)...);
     }catch (const std::exception &){
