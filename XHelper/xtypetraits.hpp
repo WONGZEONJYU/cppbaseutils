513ab41d0fcf3fd16c0e513246fc0232ecbfedd8
fuck_if <wongjy13005151320@outlook.com>
2025-07-10 00:15:28 +0800

    修正模板函数写法去掉static改成inline,减少二进制文件大小

--- XHelper/xtypetraits.hpp
+++ XHelper/xtypetraits.hpp
@@ -122,7 +122,7 @@ template<typename... Args>
 static inline constexpr auto is_const_member_function_v [[maybe_unused]] = is_const_member_function<Args...>::value;
 
 template<typename Ty>
-[[maybe_unused]] static inline auto typeName(Ty){
+[[maybe_unused]] inline auto typeName(Ty){
 #ifdef HAS_BOOST
     return boost::typeindex::type_id_with_cvr<Ty>().pretty_name();
 #else
@@ -131,7 +131,7 @@ template<typename Ty>
 }
 
 template<typename Ty>
-[[maybe_unused]] static inline auto typeName(){
+[[maybe_unused]] inline auto typeName(){
 #ifdef HAS_BOOST
     return boost::typeindex::type_id_with_cvr<Ty>().pretty_name();
 #else
