2a931ce1887423718f5369ac68b36ac6c683bc32
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2025-07-14 20:07:33 +0800

    std::ref 改 std::addressof

--- XHelper/xhelper.hpp
+++ XHelper/xhelper.hpp
@@ -191,7 +191,7 @@ template<typename Tuple, typename Pred>
 [[maybe_unused]] inline void for_each_tuple(Tuple && tuple_, Pred && pred_) {
     (void)std::apply([&pred_]<typename... Args>(Args &&... args) {
         std::size_t index{};
-        (std::forward<Pred>(pred_)(std::ref(index),std::forward<Args>(args)), ...);
+        (std::forward<Pred>(pred_)(std::addressof(index),std::forward<Args>(args)), ...);
     },std::forward<Tuple>(tuple_));
 }
 
