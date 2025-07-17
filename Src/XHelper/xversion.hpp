a350c6655b484a7744a19a33bb20eb8ed95b1855
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2025-07-17 17:24:19 +0800

    增加版本检查

--- Src/XHelper/xversion.hpp
+++ Src/XHelper/xversion.hpp
@@ -1,7 +1,18 @@
 #ifndef X_NAMESPACE_HPP
 #define X_NAMESPACE_HPP 1
 
-#define XTD_VERSION "0.0.1"
+//XTD_VERSION "0.0.1"
+
+#define XTD_VERSION_MAJOR 0
+
+#define XTD_VERSION_MINOR 0
+
+#define XTD_VERSION_PATCH 1
+
+#define XTD_VERSION_CHECK(major, minor, patch) ((major<<16)|(minor<<8)|(patch))
+
+#define XTD_VERSION XTD_VERSION_CHECK(XTD_VERSION_MAJOR,XTD_VERSION_MINOR,XTD_VERSION_PATCH)
+
 #define XTD_NAMESPACE_BEGIN namespace xtd {
 #define XTD_NAMESPACE_END }
 
