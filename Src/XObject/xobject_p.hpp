ccefc6c8c055d8d799fae5cf54dfa6a1320a3d8d
fuck_if <wongjy13005151320@outlook.com>
2025-07-23 00:57:01 +0800

    增加map和list

--- Src/XObject/xobject_p.hpp
+++ Src/XObject/xobject_p.hpp
@@ -4,21 +4,30 @@
 #include <xobject.hpp>
 #include <XTools/xpointer.hpp>
 #include <XAtomic/xatomic.hpp>
+#include <unordered_map>
+#include <list>
 
 XTD_NAMESPACE_BEGIN
 XTD_INLINE_NAMESPACE_BEGIN(v1)
 
 class XObject;
+class XConnect;
 
 class XObjectPrivate final {
 
 public:
-
     explicit XObjectPrivate() = default;
     ~XObjectPrivate() = default;
+
     static XObjectPrivate *get(XObject *o){return o->d_func();}
+
     static const XObjectPrivate *get (const XObject *o){return o->d_func();}
+
     XAtomicPointer<ExternalRefCountData> m_sharedRefcount_{};
+
+    std::unordered_map<void*,std::shared_ptr<XConnect>> ConnectData{};
+
+    std::list<std::weak_ptr<XConnect>> senders{};
 };
 
 XTD_INLINE_NAMESPACE_END
