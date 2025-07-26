df5e4fb726550dff6cca32e84d5528233cf07b08
fuck_if <wongjy13005151320@outlook.com>
2025-07-27 01:38:43 +0800

    去掉它的头文件

--- Src/XObject/xsignalslot.hpp
+++ Src/XObject/xsignalslot.hpp
@@ -4,6 +4,7 @@
 #include <XHelper/xhelper.hpp>
 #include <XAtomic/xatomic.hpp>
 #include <XObject/xobjectdefs_impl.hpp>
+#include <XObject/xfunctionaltools_impl.hpp>
 
 XTD_NAMESPACE_BEGIN
 XTD_INLINE_NAMESPACE_BEGIN(v1)
@@ -12,17 +13,17 @@ class XObject;
 
 namespace XPrivate {
 
-    class X_CLASS_EXPORT XSignalSlotBase {
+    class XSignalSlotBase {
         X_DISABLE_COPY_MOVE(XSignalSlotBase)
-    #if XTD_VERSION < XTD_VERSION_CHECK(0,0,2)
+#if XTD_VERSION < XTD_VERSION_CHECK(0,0,2)
         XAtomicInt m_ref_ {1};
         using ImplFn = void (*)(int which, XSignalSlotBase* this_, XObject *receiver, void **args, bool *ret);
         const ImplFn m_impl_{};
-    #else
+#else
         using ImplFn = void (*)(XSignalSlotBase* this_, XObject *receiver, void **args, int which, bool *ret);
         const ImplFn m_impl_;
         XAtomicInt m_ref_ {1};
-    #endif
+#endif
 
     protected:
         enum Operation {
@@ -33,79 +34,112 @@ namespace XPrivate {
         };
 
     public:
-        explicit XSignalSlotBase(ImplFn fn) noexcept;
-
         class Deleter {
         public:
-            void operator()(XSignalSlotBase * const p) const noexcept {
+            inline void operator()(XSignalSlotBase * const p) const noexcept {
                 if (p) {
                     p->destroyIfLastRef();
                 }
             }
 
-            [[maybe_unused]] static void cleanup(XSignalSlotBase * const p) noexcept {
+            [[maybe_unused]] inline static void cleanup(XSignalSlotBase * const p) noexcept {
                 Deleter{}(p);
             }
         };
 
-        bool ref() noexcept;
+        inline bool ref() noexcept {
+            return m_ref_.ref();
+        }
 
 #if XTD_VERSION < XTD_VERSION_CHECK(0,0,2)
-        void destroyIfLastRef() noexcept;
-        bool compare(void ** a);
-        void call(XObject *r, void **a);
+        inline void destroyIfLastRef() noexcept {
+            if (!m_ref_.deref()){
+                m_impl_(Destroy, this, nullptr, nullptr, nullptr);
+            }
+        }
+
+        inline bool compare(void ** const a) {
+            bool ret{};
+            m_impl_(Compare, this, nullptr, a, &ret);
+            return ret;
+        }
+
+        inline void call(XObject * const r, void ** const a) {
+            m_impl_(Call, this, r, a, nullptr);
+        }
     #else
-        void destroyIfLastRef() noexcept;
-        bool compare(void ** a);
-        void call(XObject * r, void ** a);
+        inline void destroyIfLastRef() noexcept {
+            if (!m_ref_.deref()) {
+                m_impl_(this, nullptr, nullptr, Destroy, nullptr);
+            }
+        }
+
+        inline bool compare(void ** const a) {
+            bool ret {};
+            m_impl_(this, nullptr, a, Compare, &ret);
+            return ret;
+        }
+
+        inline void call(XObject * const r, void ** const a) {
+            m_impl_(this, r, a, Call, nullptr);
+        }
     #endif
 
-        [[maybe_unused]] bool isImpl(ImplFn const f) const { return f == m_impl_; }
+        [[maybe_unused]] inline bool isImpl(ImplFn const f) const { return f == m_impl_; }
     protected:
+        explicit XSignalSlotBase(ImplFn const fn) noexcept : m_impl_(fn) {}
         ~XSignalSlotBase() = default;
     };
 
     using SlotObjUniquePtr = std::unique_ptr<XSignalSlotBase,XSignalSlotBase::Deleter>;
-    inline SlotObjUniquePtr copy(const SlotObjUniquePtr &other) noexcept {
+    inline static SlotObjUniquePtr copy(const SlotObjUniquePtr &other) noexcept {
         if (other){
             other->ref();
         }
         return SlotObjUniquePtr{other.get()};
     }
 
-    class X_CLASS_EXPORT SlotObjSharedPtr final {
+    class [[maybe_unused]] SlotObjSharedPtr final {
         SlotObjUniquePtr m_obj_{};
     public:
         SlotObjSharedPtr() noexcept = default;
 
-        [[maybe_unused]] explicit SlotObjSharedPtr(std::nullptr_t) noexcept;
+        [[maybe_unused]] explicit SlotObjSharedPtr(std::nullptr_t) noexcept :
+        SlotObjSharedPtr(){}
 
-        [[maybe_unused]] explicit SlotObjSharedPtr(SlotObjUniquePtr);
+        [[maybe_unused]] explicit SlotObjSharedPtr(SlotObjUniquePtr o):
+        m_obj_(std::move(o)){}
 
-        SlotObjSharedPtr(const SlotObjSharedPtr &other) noexcept;
+        SlotObjSharedPtr(const SlotObjSharedPtr &other) noexcept:
+        m_obj_{copy(other.m_obj_)} {}
 
-        SlotObjSharedPtr &operator=(const SlotObjSharedPtr &other) noexcept;
+        SlotObjSharedPtr &operator=(const SlotObjSharedPtr &other) noexcept{
+            auto copy{other};
+            swap(copy);
+            return *this;
+        }
 
         SlotObjSharedPtr(SlotObjSharedPtr &&other) noexcept = default;
         SlotObjSharedPtr &operator=(SlotObjSharedPtr &&other) noexcept = default;
         ~SlotObjSharedPtr() = default;
 
-        void swap(SlotObjSharedPtr &other) noexcept;
+        void swap(SlotObjSharedPtr &other) noexcept {
+            m_obj_.swap(other.m_obj_);
+        }
 
         [[nodiscard]] auto get() const noexcept{ return m_obj_.get(); }
         auto operator->() const noexcept{ return get(); }
-
         explicit operator bool() const noexcept { return static_cast<bool>(m_obj_); }
     };
 
     template <typename Func, typename Args, typename R>
-        class XCallableObject : public XSignalSlotBase,
-                                CompactStorage<std::decay_t<Func>> {
+        class XCallableObject : public XSignalSlotBase,CompactStorage<std::decay_t<Func>> {
+
         using FunctorValue = std::decay_t<Func>;
         using Storage = CompactStorage<FunctorValue>;
         using FuncType = Callable<Func, Args>;
 #if XTD_VERSION < XTD_VERSION_CHECK(0,0,2)
-        static void impl(int const which, XSignalSlotBase * const this_, XObject * const r,
+        inline static void impl(int const which, XSignalSlotBase * const this_, XObject * const r,
             void ** const a, bool * const ret) {
 #else
         static void impl(XSignalSlotBase * const this_, XObject * const r,
@@ -144,18 +178,18 @@ namespace XPrivate {
 
 #if 1
     template <typename Prototype, typename Functor> requires (countMatchingArguments<Prototype, Functor>() >= 0)
-    static constexpr XSignalSlotBase * makeCallableObject(Functor &&func) {
+    inline static constexpr XSignalSlotBase * makeCallableObject(Functor &&func) {
 #else
     template <typename Prototype, typename Functor>
-    static constexpr XSignalSlotBase * makeCallableObject(Functor &&func)
+    inline static constexpr XSignalSlotBase * makeCallableObject(Functor &&func)
     requires (countMatchingArguments<Prototype, Functor>() >= 0) {
-#endif
+#endif //1
 
-#else
+#else //__cplusplus >= 202002L
     template <typename Prototype, typename Functor>
-    static constexpr std::enable_if_t<countMatchingArguments<Prototype, Functor>() >= 0,XSignalSlotBase *>
+    inline static constexpr std::enable_if_t<countMatchingArguments<Prototype, Functor>() >= 0,XSignalSlotBase *>
     makeCallableObject(Functor &&func) {
-#endif
+#endif //__cplusplus >= 202002L
 
         using ExpectedSignature = FunctionPointer<Prototype>;
         using ExpectedReturnType = typename ExpectedSignature::ReturnType;
@@ -176,8 +210,8 @@ namespace XPrivate {
     struct AreFunctionsCompatible : std::false_type {};
 
 #if __cplusplus >= 202002L
-    template<typename Prototype, typename Functor> requires (
-        std::is_same_v<decltype(makeCallableObject<Prototype>(std::forward<Functor>(std::declval<Functor>()))),
+    template<typename Prototype, typename Functor>
+    requires (std::is_same_v<decltype(makeCallableObject<Prototype>(std::forward<Functor>(std::declval<Functor>()))),
         XSignalSlotBase *>)
     struct AreFunctionsCompatible<Prototype, Functor, XSignalSlotBase *> : std::true_type {};
 #else
@@ -188,12 +222,12 @@ namespace XPrivate {
     > : std::true_type {};
 #endif
 
-    template<typename Prototype, typename Functor>
-    inline constexpr auto AreFunctionsCompatible_v = AreFunctionsCompatible<Prototype, Functor>::value;
+    template<typename ...Args>
+    inline constexpr auto AreFunctionsCompatible_v = AreFunctionsCompatible<Args...>::value;
 
-    template<typename Prototype, typename Functor>
+    template<typename ...Args>
     [[maybe_unused]] inline constexpr auto AssertCompatibleFunctions() {
-        static_assert(AreFunctionsCompatible_v<Prototype, Functor>,
+        static_assert(AreFunctionsCompatible_v<Args...>,
                       "Functor is not compatible with expected prototype!");
         return true;
     }
