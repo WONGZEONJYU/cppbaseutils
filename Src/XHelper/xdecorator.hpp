c49a8e2ffa6cffeef145dc5095bb197b778ef297
fuck_if <wongjy13005151320@outlook.com>
2025-07-27 06:34:05 +0800

    新增修饰符头文件

---
+++ Src/XHelper/xdecorator.hpp
@@ -0,0 +1,15 @@
+#ifndef X_DECORATOR_HPP
+#define X_DECORATOR_HPP 1
+
+#define FOR_EACH_DECORATOR(op) op() op(noexcept)
+#define FOR_EACH_CVREF_DECORATOR(op)  FOR_EACH_DECORATOR(op) \
+    op(&) op(&&) \
+    op(volatile) op(volatile &) op(volatile &&)  \
+    op(const) op(const &) op(const &&) \
+    op(const volatile) op(const volatile &) op(const volatile &&) \
+    op(& noexcept) op(&& noexcept) \
+    op(volatile noexcept) op(volatile & noexcept) op(volatile && noexcept) \
+    op(const noexcept) op(const & noexcept) op(const && noexcept) \
+    op(const volatile noexcept) op(const volatile & noexcept) op(const volatile && noexcept)
+
+#endif
