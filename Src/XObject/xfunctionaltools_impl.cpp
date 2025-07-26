d92867b99194b1cea91d5b874586c9cfdfb5e85e
fuck_if <wongjy13005151320@outlook.com>
2025-07-27 01:41:06 +0800

    新增代码

--- Src/XObject/xfunctionaltools_impl.cpp
+++ Src/XObject/xfunctionaltools_impl.cpp
@@ -1,42 +1,30 @@
 #include <XObject/xfunctionaltools_impl.hpp>
 
-#if 0
+XTD_NAMESPACE_BEGIN
+XTD_INLINE_NAMESPACE_BEGIN(v1)
 
-namespace QtPrivate {
+namespace XPrivate::_testing {
+#define FOR_EACH_CVREF(op) op(&) op(const &) op(&&) op(const &&)
+    struct empty {};
+    struct final final {};
+    static_assert(std::is_same_v<CompactStorage<empty>,
+                                 detail::StorageEmptyBaseClassOptimization<empty>>);
+    static_assert(std::is_same_v<CompactStorage<final>,
+                                 detail::StorageByValue<final>>);
+    static_assert(std::is_same_v<CompactStorage<int>,
+                                 detail::StorageByValue<int>>);
 
-#define FOR_EACH_CVREF(op) \
-op(&) \
-op(const &) \
-op(&&) \
-op(const &&) \
-/* end */
+    #define CHECK1(Obj, cvref) static_assert(std::is_same_v<decltype(std::declval<CompactStorage< Obj > cvref>().object()), \
+        Obj cvref>);
 
-    namespace _testing {
-        struct empty {};
-        struct final final {};
-        static_assert(std::is_same_v<CompactStorage<empty>,
-                                     detail::StorageEmptyBaseClassOptimization<empty>>);
-        static_assert(std::is_same_v<CompactStorage<final>,
-                                     detail::StorageByValue<final>>);
-        static_assert(std::is_same_v<CompactStorage<int>,
-                                     detail::StorageByValue<int>>);
-#define CHECK1(Obj, cvref) \
-static_assert(std::is_same_v<decltype(std::declval<CompactStorage< Obj > cvref>().object()), \
-Obj cvref>);
-#define CHECK(cvref) \
-CHECK1(empty, cvref) \
-CHECK1(final, cvref) \
-CHECK1(int, cvref) \
-/* end */
+    #define CHECK(cvref) CHECK1(empty, cvref) CHECK1(final, cvref) CHECK1(int, cvref)
 
-        FOR_EACH_CVREF(CHECK)
+    FOR_EACH_CVREF(CHECK)
+
     #undef CHECK
     #undef CHECK1
-    } // namespace _testing
-
-} // namespace QtPrivate
-
 #undef FOR_EACH_CVREF
+} // namespace XPrivate::_testing
 
-#endif
-
+XTD_INLINE_NAMESPACE_END
+XTD_NAMESPACE_END
--- Src/XObject/xfunctionaltools_impl.hpp
+++ Src/XObject/xfunctionaltools_impl.hpp
@@ -1,39 +1,62 @@
 #ifndef XFUNCTIONALTOOLS_IMPL_HPP
 #define XFUNCTIONALTOOLS_IMPL_HPP
 
-#if 0
+#include <XHelper/xversion.hpp>
+#include <utility>
 
-namespace detail {
+XTD_NAMESPACE_BEGIN
+XTD_INLINE_NAMESPACE_BEGIN(v1)
+
+namespace XPrivate {
+
+    namespace detail {
+
 #define FOR_EACH_CVREF(op) op(&) op(const &) op(&&) op(const &&)
 
-    template<typename Object, typename = void>
-    class [[maybe_unused]] StorageByValue {
+        template<typename Object, typename = void>
+        class [[maybe_unused]] StorageByValue {
         public:
-        Object o;
-#define MAKE_GETTER(cvRef) \
-constexpr Object cvRef object() cvRef noexcept \
-{ return static_cast<Object cvRef>(o); }
-        FOR_EACH_CVREF(MAKE_GETTER)
-#undef MAKE_GETTER
-    };
+            Object o;
+            #define MAKE_GETTER(cvRef) \
+            constexpr Object cvRef object() cvRef noexcept \
+            { return static_cast<Object cvRef>(o); }
 
-    template <typename Object, typename = void>
-    class [[maybe_unused]] StorageEmptyBaseClassOptimization : Object {
+            FOR_EACH_CVREF(MAKE_GETTER)
+            #undef MAKE_GETTER
+        };
+
+        template<typename Object, typename = void>
+        class [[maybe_unused]] StorageEmptyBaseClassOptimization : Object {
         public:
-        StorageEmptyBaseClassOptimization() = default;
-        explicit StorageEmptyBaseClassOptimization(Object &&o) noexcept : Object(std::move(o)) {}
-        explicit StorageEmptyBaseClassOptimization(const Object &o) : Object(o) {}
-#define MAKE_GETTER(cvRef) \
-constexpr Object cvRef object() cvRef noexcept \
-{ return static_cast<Object cvRef>(*this); }
-        FOR_EACH_CVREF(MAKE_GETTER)
-#undef MAKE_GETTER
-    };
-#undef FOR_EACH_CVREF
-} //namespace detail
+            StorageEmptyBaseClassOptimization() = default;
 
+            explicit StorageEmptyBaseClassOptimization(Object &&o) noexcept: Object(std::move(o)) {}
 
+            explicit StorageEmptyBaseClassOptimization(const Object &o) : Object(o) {}
 
-#endif
+            #define MAKE_GETTER(cvRef) \
+            constexpr Object cvRef object() cvRef noexcept \
+            { return static_cast<Object cvRef>(*this); }
+
+            FOR_EACH_CVREF(MAKE_GETTER)
+
+            #undef MAKE_GETTER
+            #undef FOR_EACH_CVREF
+        };
+
+    } //namespace detail
+
+    template <typename Object, typename Tag = void>
+    using CompactStorage [[maybe_unused]] = std::conditional_t<
+            std::conjunction_v<std::is_empty<Object>, //逻辑& and
+                    std::negation<std::is_final<Object>> //逻辑! not
+            >,
+            detail::StorageEmptyBaseClassOptimization<Object, Tag>,
+            detail::StorageByValue<Object, Tag>
+    >;
+}
+
+XTD_INLINE_NAMESPACE_END
+XTD_NAMESPACE_END
 
 #endif
