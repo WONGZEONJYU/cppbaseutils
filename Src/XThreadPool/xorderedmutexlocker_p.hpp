820d0daece327693e14562cae08aa2b710bae745
fuck_if <wongjy13005151320@outlook.com>
2025-07-27 01:44:25 +0800

    没有改变功能

--- Src/XThreadPool/xorderedmutexlocker_p.hpp
+++ Src/XThreadPool/xorderedmutexlocker_p.hpp
@@ -1,16 +1,16 @@
 #ifndef X_ORDERED_MUTEX_LOCKER_HPP
 #define X_ORDERED_MUTEX_LOCKER_HPP 1
 
-#include <mutex>
 #include <XHelper/xhelper.hpp>
+#include <mutex>
 
 class XOrderedMutexLocker {
     std::mutex *m_mtx1_{},*m_mtx2_{};
     bool m_locked_{};
 public:
     XOrderedMutexLocker(std::mutex * const m1, std::mutex * const m2)
-        : m_mtx1_(m1 == m2 ? m1 : std::less<std::mutex *>()(m1, m2) ? m1 : m2),
-          m_mtx2_(m1 == m2 ?  nullptr : std::less<std::mutex *>()(m1, m2) ? m2 : m1) {
+        : m_mtx1_(m1 == m2 ? m1 : std::less<>()(m1, m2) ? m1 : m2),
+          m_mtx2_(m1 == m2 ?  nullptr : std::less<>()(m1, m2) ? m2 : m1) {
         relock();
     }
 
@@ -46,7 +46,7 @@ public:
         }
     }
 
-    void dismiss() {
+    [[maybe_unused]] void dismiss() {
         X_ASSERT(m_locked_);
         m_locked_ = false;
     }
@@ -59,7 +59,7 @@ public:
         }
     }
 
-    static bool relock(std::mutex * const mtx1, std::mutex * const mtx2) {
+    [[maybe_unused]] inline static bool relock(std::mutex * const mtx1, std::mutex * const mtx2) {
         // mtx1 is already locked, mtx2 not... do we need to unlock and relock?
         if (mtx1 == mtx2){
             return {};
