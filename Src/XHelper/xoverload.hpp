85f2e2da13aa85a04a568dcd0e5f236cbbd29b0f
fuck_if <wongjy13005151320@outlook.com>
2025-07-27 04:58:12 +0800

    小调整

--- Src/XHelper/xoverload.hpp
+++ Src/XHelper/xoverload.hpp
@@ -24,7 +24,7 @@ op(&) op(&&) \
 op(volatile) op(volatile &) op(volatile &&)  \
 op(const) op(const &) op(const &&) \
 op(const volatile) op(const volatile &) op(const volatile &&) \
-op(&& noexcept) op(& noexcept) \
+op(& noexcept) op(&& noexcept) \
 op(volatile noexcept) op(volatile & noexcept) op(volatile && noexcept)  \
 op(const noexcept) op(const & noexcept) op(const && noexcept) \
 op(const volatile noexcept) op(const volatile & noexcept) op(const volatile && noexcept) \
