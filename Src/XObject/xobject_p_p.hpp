0aca4949f7ccbe309fd74d7d7f334298941ee63a
fuck_if <wongjy13005151320@outlook.com>
2025-07-27 15:38:39 +0800

    增加获取发送者指针和index

--- Src/XObject/xobject_p_p.hpp
+++ Src/XObject/xobject_p_p.hpp
@@ -10,19 +10,13 @@
 XTD_NAMESPACE_BEGIN
 XTD_INLINE_NAMESPACE_BEGIN(v1)
 
-class XObjectPrivate::XSender final {
-public:
-    explicit XSender() = default;
-    ~XSender() = default;
-};
-
 class XObjectPrivate::XConnection final : public std::enable_shared_from_this<XConnection> {
     friend class XObject;
     X_DISABLE_COPY_MOVE(XConnection)
 public:
-    XObject * m_sender{};
+    [[maybe_unused]] XObject * m_sender{};
     XAtomicPointer<XObject> m_receiver{};
-    std::size_t m_signal_index{};
+    [[maybe_unused]] std::size_t m_signal_index{};
     XPrivate::XSignalSlotBase * m_slot_raw{};
     uint m_isSlotObject:1;
 
@@ -47,7 +41,7 @@ public:
     XAtomicInt m_ref{};
     std::unordered_map<std::size_t,XConnectionList> m_signalVector;
     std::list<XConnection_WPtr> m_senders{};
-    XSender * m_currentSenders{};
+    XSender * m_currentSender{};
 
     inline void resizeSignalVector(std::size_t const signal_index){
         if (!m_signalVector.contains(signal_index)){
@@ -58,6 +52,36 @@ public:
     inline XConnectionList& connectionsForSignal(size_t const signal_index) {
         return m_signalVector[signal_index];
     }
+};
+
+class XObjectPrivate::XSender final {
+public:
+    [[maybe_unused]] XSender(XObject * const receiver, XObject * const sender, std::size_t const signal, XConnectionData * const receiverConnections)
+            : m_receiver(receiver), m_sender(sender), m_signal(signal)
+    {
+        if (receiverConnections) {
+            m_previous = receiverConnections->m_currentSender;
+            receiverConnections->m_currentSender = this;
+        }
+    }
+
+    ~XSender(){
+        if (m_receiver){
+            m_receiver->d_func()->m_connections.loadAcquire()->m_currentSender = m_previous;
+        }
+    }
+
+    [[maybe_unused]] void receiverDeleted(){
+        XSender *s = this;
+        while (s) {
+            s->m_previous = {};
+            s = s->m_previous;
+        }
+    }
+
+    XSender *m_previous{};
+    [[maybe_unused]] XObject *m_receiver{},*m_sender{};
+    [[maybe_unused]] std::size_t m_signal{};
 };
 
 XTD_INLINE_NAMESPACE_END
