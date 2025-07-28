6aa0aded5d343d1178abdde1b29b1a571657f7de
fuck_if <wongjy13005151320@outlook.com>
2025-07-28 15:29:11 +0800

    删除导入导出宏

--- Src/XHelper/xhelper.hpp
+++ Src/XHelper/xhelper.hpp
@@ -29,10 +29,10 @@
     X_DEFAULT_COPY(__VA_ARGS__)\
     X_DEFAULT_MOVE(__VA_ARGS__)
 
-template <typename T> X_API inline T *xGetPtrHelper(T *ptr) noexcept { return ptr; }
-template <typename Ptr> X_API inline auto xGetPtrHelper(Ptr &ptr) noexcept -> decltype(ptr.get())
+template <typename T> inline T *xGetPtrHelper(T *ptr) noexcept { return ptr; }
+template <typename Ptr> inline auto xGetPtrHelper(Ptr &ptr) noexcept -> decltype(ptr.get())
 { static_assert(noexcept(ptr.get()), "Smart d pointers for X_DECLARE_PRIVATE must have noexcept get()"); return ptr.get(); }
-template <typename Ptr> X_API inline auto xGetPtrHelper(Ptr const &ptr) noexcept -> decltype(ptr.get())
+template <typename Ptr> inline auto xGetPtrHelper(Ptr const &ptr) noexcept -> decltype(ptr.get())
 { static_assert(noexcept(ptr.get()), "Smart d pointers for X_DECLARE_PRIVATE must have noexcept get()"); return ptr.get(); }
 
 #define X_DECLARE_PRIVATE(Class) \
@@ -74,7 +74,7 @@ XTD_NAMESPACE_BEGIN
 XTD_INLINE_NAMESPACE_BEGIN(v1)
 
 template<typename F>
-class X_TEMPLATE_EXPORT [[maybe_unused]]  Destroyer final {
+class [[maybe_unused]]  Destroyer final {
     X_DISABLE_COPY_MOVE(Destroyer)
     mutable F m_fn_{};
     mutable uint32_t m_is_destroy:1;
@@ -100,7 +100,7 @@ public:
 };
 
 template<typename F2>
-class X_TEMPLATE_EXPORT [[maybe_unused]] X_RAII final {
+class [[maybe_unused]] X_RAII final {
     X_DISABLE_COPY_MOVE(X_RAII)
     mutable F2 m_f2_{};
     mutable uint32_t m_is_destroy_:1;
@@ -160,7 +160,7 @@ X_API void x_assert_what(const std::string_view &, const std::string_view &what,
  * @return std::unique_ptr<T>
  */
 template<typename T, typename ... Args>
-[[maybe_unused]] [[nodiscard]] X_API inline std::unique_ptr<T> make_Unique(Args && ...args) noexcept {
+[[maybe_unused]] [[nodiscard]] inline std::unique_ptr<T> make_Unique(Args && ...args) noexcept {
     try{
         return std::make_unique<T>(std::forward<Args>(args)...);
     }catch (const std::exception &){
@@ -176,7 +176,7 @@ template<typename T, typename ... Args>
  * @return std::shared_ptr<T>
  */
 template<typename T, typename ... Args>
-[[maybe_unused]] [[nodiscard]] X_API inline std::shared_ptr<T> make_Shared(Args && ...args) noexcept {
+[[maybe_unused]] [[nodiscard]] inline std::shared_ptr<T> make_Shared(Args && ...args) noexcept {
     try{
         return std::make_shared<T>(std::forward<Args>(args)...);
     }catch (const std::exception &){
@@ -195,7 +195,7 @@ template<typename T, typename ... Args>
  * @param pred_
  */
 template<typename Tuple, typename Pred>
-[[maybe_unused]] X_API inline void for_each_tuple(Tuple && tuple_, Pred && pred_) {
+[[maybe_unused]] inline void for_each_tuple(Tuple && tuple_, Pred && pred_) {
     (void)std::apply([&pred_]<typename... Args>(Args &&... args) {
         std::size_t index{};
         (std::forward<Pred>(pred_)(std::ref(index),std::forward<Args>(args)), ...);
@@ -204,7 +204,7 @@ template<typename Tuple, typename Pred>
 
 namespace TuplePrivate {
     template<const std::size_t N,typename... Args>
-    X_API inline auto Left_Tuple_n_(const std::tuple<Args...> & tuple_) {
+    inline auto Left_Tuple_n_(const std::tuple<Args...> & tuple_) {
         static_assert(N <= sizeof...(Args), "N must be <= tuple size");
         static_assert(N <= std::tuple_size_v<std::decay_t<decltype(tuple_)>>, "N must be <= tuple size");
         return [&]<const std::size_t... I>(std::index_sequence<I...>)
@@ -215,7 +215,7 @@ namespace TuplePrivate {
 
     // 方法1: 获取后N个元素的通用模板
     template<const std::size_t N, typename... Args>
-    X_API inline auto Last_Tuple_n_(const std::tuple<Args...> & tuple_) {
+    inline auto Last_Tuple_n_(const std::tuple<Args...> & tuple_) {
         static_assert(N <= sizeof...(Args), "N must be <= tuple size");
         static_assert(N <= std::tuple_size_v<std::decay_t<decltype(tuple_)>>,"N must be <= tuple size");
         constexpr auto S{sizeof...(Args)  - N};
@@ -227,7 +227,7 @@ namespace TuplePrivate {
 
     // 方法3: 跳过前面元素,取剩余的
     template<std::size_t Skip, typename... Args>
-    X_API inline auto skip_front_n_(const std::tuple<Args...>& tuple_) {
+    inline auto skip_front_n_(const std::tuple<Args...>& tuple_) {
         static_assert(Skip <= sizeof...(Args), "Skip count exceeds tuple size");
         constexpr auto Offset{Skip},ReCount{sizeof...(Args) - Skip};
         static_assert(Offset + ReCount <= sizeof...(Args), "Range out of bounds");
@@ -246,7 +246,7 @@ namespace TuplePrivate {
  * @return
  */
 template<const std::size_t N,typename Tuple>
-[[maybe_unused]] X_API inline auto Left_Tuple(const Tuple & tuple_) {
+[[maybe_unused]] inline auto Left_Tuple(const Tuple & tuple_) {
     return TuplePrivate::Left_Tuple_n_<N>(tuple_);
 }
 
@@ -258,7 +258,7 @@ template<const std::size_t N,typename Tuple>
  * @return decltype(TuplePrivate::Last_Tuple_n_<N,Tuple>(tuple_))
  */
 template<const std::size_t N,typename Tuple>
-[[maybe_unused]] X_API inline auto Last_Tuple(const Tuple & tuple_) {
+[[maybe_unused]] inline auto Last_Tuple(const Tuple & tuple_) {
     return TuplePrivate::Last_Tuple_n_<N>(tuple_);
 }
 
@@ -270,7 +270,7 @@ template<const std::size_t N,typename Tuple>
  * @return decltype(TuplePrivate::skip_front_n_<Skip>(tuple_))
  */
 template<const std::size_t N,typename Tuple>
-[[maybe_unused]] X_API inline auto SkipFront_Tuple(const Tuple &tuple_) {
+[[maybe_unused]] inline auto SkipFront_Tuple(const Tuple &tuple_) {
     return TuplePrivate::skip_front_n_<N>(tuple_);
 }
 
