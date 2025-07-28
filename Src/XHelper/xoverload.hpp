0c13f3ccb8f01c80eb27410a8445b701734b708d
fuck_if <wongjy13005151320@outlook.com>
2025-07-28 15:27:19 +0800

    没有改变功能

--- Src/XHelper/xoverload.hpp
+++ Src/XHelper/xoverload.hpp
@@ -30,7 +30,7 @@ public:
         inline static constexpr auto of(R(C::*ptr)(Args...) __VA_ARGS__) noexcept -> decltype(ptr) \
         { return ptr; }
 
-    FOR_EACH_CVREF_DECORATOR(MAKE_OPERATOR_AND_OF)
+    FOR_EACH_CVREF_DECORATOR_NOEXCEPT(MAKE_OPERATOR_AND_OF)
     #undef MAKE_OPERATOR_AND_OF
 };
 
