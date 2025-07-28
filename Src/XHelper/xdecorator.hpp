da059587d193eb65e2a08902b3c123126c86f9d9
fuck_if <wongjy13005151320@outlook.com>
2025-07-28 15:29:26 +0800

    分开写

--- Src/XHelper/xdecorator.hpp
+++ Src/XHelper/xdecorator.hpp
@@ -2,11 +2,16 @@
 #define X_DECORATOR_HPP 1
 
 #define FOR_EACH_DECORATOR(op) op() op(noexcept)
-#define FOR_EACH_CVREF_DECORATOR(op) FOR_EACH_DECORATOR(op) \
+
+#define FOR_EACH_CVREF_DECORATOR(op)\
     op(&) op(&&) \
     op(volatile) op(volatile &) op(volatile &&)  \
     op(const) op(const &) op(const &&) \
-    op(const volatile) op(const volatile &) op(const volatile &&) \
+    op(const volatile) op(const volatile &) op(const volatile &&)
+
+#define FOR_EACH_CVREF_DECORATOR_NOEXCEPT(op) \
+    FOR_EACH_DECORATOR(op) \
+    FOR_EACH_CVREF_DECORATOR(op) \
     op(& noexcept) op(&& noexcept) \
     op(volatile noexcept) op(volatile & noexcept) op(volatile && noexcept) \
     op(const noexcept) op(const & noexcept) op(const && noexcept) \
