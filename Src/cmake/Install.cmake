fe76da78861419b108502b28bff3a02404e02092
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2025-09-12 20:18:27 +0800

    new add XMemory

--- Src/cmake/Install.cmake
+++ Src/cmake/Install.cmake
@@ -94,6 +94,15 @@ install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/XMath/
         PATTERN "*_private.hpp" EXCLUDE
 )
 
+install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/XMemory/
+        DESTINATION include/XMemory
+        FILES_MATCHING
+        PATTERN "*.h"
+        PATTERN "*.hpp"
+        PATTERN "*_p.hpp" EXCLUDE
+        PATTERN "*_private.hpp" EXCLUDE
+)
+
 # 导出库配置供其他项目使用
 install(EXPORT ${PROJECT_NAME}Targets
     FILE ${PROJECT_NAME}Targets.cmake
