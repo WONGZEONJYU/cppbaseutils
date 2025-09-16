81c3dee042b999382efeb07ed2bb69787dcf46e9
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2025-09-16 17:25:24 +0800

    增加顺序容器尾插辅助函数

--- Src/XContainerHelper/xcontainerhelper.hpp
+++ Src/XContainerHelper/xcontainerhelper.hpp
@@ -14,6 +14,22 @@ auto sliced(Con_ const & c,std::size_t const s,std::size_t const e) noexcept -> 
     return { subRang.cbegin() , subRang.cend() };
 }
 
+template<typename Con_>
+auto append(Con_ & c,typename Con_::value_type const & v) noexcept -> Con_ &
+{ c.push_back(std::forward<decltype(v)>(v)); return c; }
+
+template<typename Con_>
+auto append(Con_ & c,typename Con_::value_type && v) noexcept ->Con_ &
+{ c.push_back(std::forward<decltype(v)>(v)); return c; }
+
+template<typename Con_,typename Con_R >
+auto append(Con_ & fst ,Con_R const & snd) noexcept -> Con_ &
+{ fst.insert(fst.cend(),snd.cbegin(),snd.cend()); return fst; }
+
+template<typename Con_>
+auto append(Con_ & c ,const typename Con_::value_type * const d,std::size_t const length) noexcept ->Con_ &
+{ return append(c,std::ranges::subrange{d, d + length } ); }
+
 XTD_INLINE_NAMESPACE_END
 XTD_NAMESPACE_END
 
