afd9200fbfb1487b21475cfaa00c4159d2cfa607
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2025-09-17 14:07:51 +0800

    返回值位于后置,增加constexpr修饰

--- Src/XContainerHelper/xcontainerhelper.hpp
+++ Src/XContainerHelper/xcontainerhelper.hpp
@@ -7,24 +7,31 @@ XTD_NAMESPACE_BEGIN
 XTD_INLINE_NAMESPACE_BEGIN(v1)
 
 template <typename Con_>
-auto sliced(Con_ const & c,std::size_t const s,std::size_t const e) noexcept -> Con_ {
+requires std::ranges::range<Con_>
+constexpr auto sliced(Con_ const & c,std::size_t const s,std::size_t const e) noexcept -> Con_ {
     if (s >= c.size() ) { return {}; }
     auto const start { c.cbegin() + s };
     auto const subRang { std::ranges::subrange(start, start + std::ranges::min(e,c.size())) };
     return { subRang.cbegin() , subRang.cend() };
 }
 
-template<typename Con_,typename ...Args>
-auto append(Con_ & c,Args && ...args) noexcept ->Con_ &
-{ c.emplace_back(std::forward<decltype(args)>(args)...); return c; }
+template<typename Con_>
+constexpr auto append(Con_ & c,typename Con_::const_reference v) noexcept -> Con_ &
+{ c.push_back(v); return c; }
+
+template<typename Con_>
+constexpr auto append(Con_ & c,typename Con_::value_type && v) noexcept -> Con_ &
+{ c.push_back(std::forward<decltype(v)>(v)); return c; }
 
 template<typename Con_,typename Con_R >
-auto append(Con_ & fst ,Con_R const & snd) noexcept -> Con_ &
+requires std::ranges::range<Con_> && std::ranges::input_range<Con_R>
+constexpr auto append(Con_ & fst ,Con_R const & snd) noexcept -> Con_ &
 { fst.insert(fst.cend(),snd.cbegin(),snd.cend()); return fst; }
 
 template<typename Con_>
-auto append(Con_ & c ,typename Con_::const_pointer const d,std::size_t const length) noexcept ->Con_ &
-{ return append(c,std::ranges::subrange {d, d + length }); }
+requires std::ranges::range<Con_>
+constexpr auto append(Con_ & c , typename Con_::const_pointer const d,std::size_t const length) noexcept -> Con_ &
+{ return append(c,std::ranges::subrange{d, d + length } ); }
 
 XTD_INLINE_NAMESPACE_END
 XTD_NAMESPACE_END
