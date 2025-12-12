20170802beaf5fc70664db3709480a49396ce628
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2025-12-12 12:00:38 +0800

    2025-12-11 update

--- Src/XMath/xmath.hpp
+++ Src/XMath/xmath.hpp
@@ -3,6 +3,7 @@
 
 #include <utility>
 #include <XHelper/xversion.hpp>
+#include <cstdint>
 
 XTD_NAMESPACE_BEGIN
 XTD_INLINE_NAMESPACE_BEGIN(v1)
@@ -17,7 +18,7 @@ template<typename = double> struct Range;
 template<typename T>
 struct Range final {
 
-    static_assert(std::is_floating_point_v<T>,"T must be floating point!");
+    static_assert(std::is_arithmetic_v<T>,"T must be arithmetic!");
 
     using RangeType = std::pair<T,T>;
 
@@ -34,29 +35,31 @@ struct Range final {
     :m_range_{range},m_eps_(eps),m_left(left),m_right(right) {}
 
     constexpr bool leftCmp(T const value) const noexcept {
-        return BoundType::Closed == m_left
-                     ? value >= m_range_.first - m_eps_
-                     : value >  m_range_.first + m_eps_;
+        if constexpr (std::is_floating_point_v<T>) {
+            return BoundType::Closed == m_left
+                 ? value >= m_range_.first - m_eps_
+                 : value >  m_range_.first + m_eps_;
+        }else {
+            return BoundType::Closed == m_left
+                 ? value >= m_range_.first
+                 : value >  m_range_.first;
+        }
     }
 
     constexpr bool rightCmp(T const value) const noexcept {
-        return BoundType::Closed == m_right
-                     ? value <= m_range_.second + m_eps_
-                     : value <  m_range_.second - m_eps_;
+        if constexpr (std::is_floating_point_v<T>) {
+            return BoundType::Closed == m_right
+                 ? value <= m_range_.second + m_eps_
+                 : value <  m_range_.second - m_eps_;
+        }else {
+            return BoundType::Closed == m_right
+                 ? value <= m_range_.second
+                 : value <  m_range_.second;
+        }
     }
 
     [[nodiscard]] constexpr bool contains(T const value) const noexcept {
-#if 0
-        auto const leftOk{ BoundType::Closed == m_left
-                     ? value >= m_range_.first - m_eps_
-                     : value >  m_range_.first + m_eps_ };
-
-        auto const rightOk{ BoundType::Closed == m_right
-                     ? value <= m_range_.second + m_eps_
-                     : value <  m_range_.second - m_eps_ };
-#else
-        auto const leftOk { leftCmp(value) }, rightOk{ rightCmp(value) };
-#endif
+        auto const leftOk{ leftCmp(value) },rightOk{ rightCmp(value) };
         return leftOk && rightOk;
     }
 
@@ -70,11 +73,23 @@ private:
 };
 
 template<typename T> Range(T, T) -> Range<T>;
-
 template<typename T> Range(std::pair<T, T>) -> Range<T>;
 
 using RangeDouble = Range<>;
 using RangeFloat = Range<float>;
+using RangeChar = Range<char>;
+using RangeSignedChar = Range<signed char>;
+using RangeUnsignedChar = Range<unsigned char>;
+using RangeShort = Range<short>;
+using RangeUnsignedShort = Range<unsigned short>;
+using RangeInt = Range<int>;
+using RangeUnsignedInt = Range<unsigned int>;
+using RangeLong = Range<long>;
+using RangeUnsignedLong = Range<unsigned long>;
+using RangeLongLong = Range<long long>;
+using RangeUnsignedLongLong = Range<unsigned long long>;
+using RangeInt64 = Range<int64_t>;
+using RangeUInt64 = Range<uint64_t>;
 
 XTD_INLINE_NAMESPACE_END
 XTD_NAMESPACE_END
