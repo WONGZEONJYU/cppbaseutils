0521bc093900e39e561c6c31a132974d1585dbe5
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2026-01-02 19:26:13 +0800

    2026 01-02 update

--- Test/CoroutineTest/coroutine1.hpp
+++ Test/CoroutineTest/coroutine1.hpp
@@ -3,80 +3,120 @@
 
 #include <iostream>
 #include <coroutine>
-#include <exception>
 #include <XGlobal/xclasshelpermacros.hpp>
 
-template<std::movable>
-struct coroutine {
+template<typename Promise>
+struct Generator {
 
-    struct promise;
-    using promise_type = promise;
+    using promise_type = Promise;
+    using coroutine_handle = std::coroutine_handle<promise_type>;
 
 private:
-    using coroutine_handle = std::coroutine_handle<promise_type>;
-    coroutine_handle m_coh{};
+    coroutine_handle m_coroHandle_ {};
+    bool m_isDestroy_ {};
+    bool m_autoDestroy_ { true };
 
 public:
-    [[nodiscard]] auto & promise() const
-    { return m_coh.promise(); }
+    [[nodiscard]] constexpr auto promiseRef() const
+    { return std::addressof(m_coroHandle_.promise()); }
 
-    void resume() const
-    { m_coh.resume(); }
+    constexpr void resume() const
+    { if (m_coroHandle_) { m_coroHandle_.resume(); } }
 
-    void operator()() const
-    { m_coh.operator()(); }
+    constexpr void operator()() const
+    { resume(); }
 
     [[nodiscard]] bool done() const
-    { return m_coh.done(); }
+    { return m_coroHandle_.done(); }
 
     explicit constexpr operator bool() const noexcept
-    { return m_coh.operator bool(); }
+    { return m_coroHandle_.operator bool(); }
 
-    constexpr coroutine(coroutine_handle const h)
-    : m_coh { h } {}
+    constexpr void isNeedAutoDestroy(bool const b = true) noexcept
+    { m_autoDestroy_ = b; }
 
-    constexpr void swap(coroutine & o) noexcept {
-        if (this != std::addressof(o)) {
-            if (m_coh) { m_coh.destroy(); }
-            m_coh = o.m_coh;
-            o.m_coh = {};
+    constexpr bool isAutoDestroy() const noexcept
+    { return m_autoDestroy_; }
+
+    constexpr void destroy()
+        noexcept( noexcept(std::declval<coroutine_handle>().destroy()) )
+    {
+        if (m_coroHandle_ && !m_isDestroy_) {
+            while (!m_coroHandle_.done()) { m_coroHandle_(); }
+            m_isDestroy_ = true;
+            m_coroHandle_.destroy();
         }
     }
 
-    coroutine(coroutine && o) noexcept
+    constexpr Generator() noexcept = default;
+
+    constexpr Generator(coroutine_handle const h) noexcept
+    : m_coroHandle_ { h } {}
+
+    X_DISABLE_COPY(Generator)
+
+    Generator(Generator && o) noexcept
     { swap(o); }
 
-    coroutine& operator= (coroutine && o) noexcept
+    Generator& operator= (Generator && o) noexcept
     { swap(o); return *this; }
 
-    virtual ~coroutine()
-    { if (m_coh) { m_coh.destroy(); } }
+    constexpr void swap(Generator & o) noexcept {
+        if (this != std::addressof(o)) {
+            if (m_coroHandle_) { m_coroHandle_.destroy(); }
+            m_coroHandle_ = o.m_coroHandle_;
+            m_isDestroy_ = o.m_isDestroy_;
+            m_autoDestroy_ = o.m_autoDestroy_;
+            o.m_coroHandle_ = {};
+            o.m_isDestroy_ = {};
+            o.m_autoDestroy_ = true;
+        }
+    }
 
-    X_DISABLE_COPY(coroutine)
+    virtual ~Generator() { if (isAutoDestroy()) { destroy(); } }
 
+    friend bool operator==(Generator const & lhs, Generator const & rhs) noexcept
+    { return lhs.m_coroHandle_ == rhs.m_coroHandle_; }
+
+    friend std::strong_ordering operator<=>(Generator const & lhs, Generator const & rhs) noexcept {
+        return lhs.m_autoDestroy_ <=> rhs.m_autoDestroy_;
+    }
+
+    static constexpr auto from_promise(promise_type * const p) noexcept
+    { return coroutine_handle::from_promise(*p); }
+
+    static constexpr auto from_address(void * const p) noexcept
+    { return coroutine_handle::from_address(p); }
+
+    explicit operator std::coroutine_handle<> () const noexcept {
+        return static_cast< std::coroutine_handle<> > ( m_coroHandle_ );
+    }
+
+    friend struct std::hash<Generator>;
 };
 
+template<typename Promise>
+constexpr void swap(Generator<Promise> & lhs,Generator<Promise> & rhs) noexcept
+{ lhs.swap(rhs); }
 
-template<std::movable T>
-struct coroutine<T>::promise {
+template <typename Promise>
+struct std::hash<Generator<Promise>> {
 
-    T m_value;
+    constexpr size_t operator()(Generator<Promise> const & coro) const noexcept {
 
-    coroutine get_return_object ()
-    { return { coroutine_handle::from_promise(*this)}; }
+        std::size_t const h[] {
+            std::hash<void*>{}(coro.m_coroHandle_.address())
+            ,std::hash<bool>{}(coro.m_isDestroy_)
+            , std::hash<bool>{}(coro.m_autoDestroy_)
+        };
 
-    [[nodiscard]] std::suspend_always initial_suspend() const noexcept
-    { (void)this; return {}; }
+        static auto const hash_combine { [](std::size_t & seed, std::size_t const value) noexcept
+        { seed ^= value + 0x9e3779b97f4a7c15ULL + (seed << 6) + (seed >> 2); } };
 
-    [[nodiscard]] std::suspend_always final_suspend() const noexcept
-    { (void)this; return {}; }
-
-
-
-    void unhandled_exception() const noexcept { (void)this;std::terminate(); }
+        std::size_t seed{};
+        for (auto && item : h) { hash_combine(seed, item); }
+        return seed;
+    }
 };
-
-
-
 
 #endif
--- Test/CoroutineTest/main.cpp
+++ Test/CoroutineTest/main.cpp
@@ -1,12 +1,163 @@
 #include <iostream>
 #include <coroutine1.hpp>
+#include <future>
+#include <thread>
+#include <XHelper/xhelper.hpp>
 
-int main()
+template<typename T>
+struct promise {
+
+    T m_value{};
+
+    using coroutine_handle_type = Generator<promise>;
+
+    auto get_return_object ()
+    { return coroutine_handle_type::from_promise(this); }
+
+    static std::suspend_always initial_suspend() noexcept
+    { return {}; }
+
+    static auto final_suspend() noexcept {
+
+        struct awaiter {
+            static constexpr bool await_ready() noexcept { return {}; }
+            static constexpr auto await_suspend(std::coroutine_handle<> const coro)  noexcept
+            { if (coro) { while (!coro.done()) { coro.resume(); } } }
+            static constexpr void await_resume() noexcept {}
+        };
+
+        return awaiter{};
+    }
+
+    void return_value(T v) noexcept
+    { m_value = std::move(v); }
+
+    std::suspend_always yield_value(T v) noexcept
+    { m_value = std::move(v); return {}; }
+
+    void unhandled_exception() const noexcept { (void)this;std::terminate(); }
+
+    static coroutine_handle_type get_return_object_on_allocation_failure()
+    { return {}; }
+
+    void * operator new(std::size_t const n) noexcept
+    { return std::malloc(n); }
+
+    void operator delete(void * const p) noexcept
+    { std::free(p); }
+};
+
+[[maybe_unused]] static Generator<promise<int>> f1() {
+    std::cout << FUNC_SIGNATURE << " begin" << std::endl;
+    for (int i{1};i <= 10; ++i) { co_yield i; }
+    std::cout << FUNC_SIGNATURE << " end" << std::endl;
+    co_return -1;
+}
+
+[[maybe_unused]] static void testF1() {
+    std::cout << FUNC_SIGNATURE << " begin" << std::endl;
+    for (auto const ch{ f1() };!ch.done();) {
+        ch();
+        std::cout << std::dec << ch.promiseRef()->m_value << std::endl;
+    }
+    std::cout << FUNC_SIGNATURE << " end" << std::endl;
+}
+
+#if 1
+template<std::movable T>
+struct promise< std::future<T> > {
+
+    using coroutine_handle_type = Generator<promise>;
+    using future = std::future<T>;
+    future m_finalValue {};
+
+    auto get_return_object()
+    { return coroutine_handle_type::from_promise(this); }
+
+    static auto initial_suspend() noexcept
+    { return std::suspend_always{}; }
+
+    static auto final_suspend() noexcept {
+
+        struct awaiter {
+            static constexpr bool await_ready() noexcept { return {}; }
+            static constexpr auto await_suspend(std::coroutine_handle<> const coro)  noexcept
+            { if (coro) { while (!coro.done()) { coro.resume(); } } }
+            static constexpr void await_resume() noexcept {}
+        };
+
+        return awaiter {};
+    }
+
+    static void return_void() noexcept{}
+
+    void unhandled_exception() const noexcept { (void)this; std::terminate(); }
+
+    static coroutine_handle_type get_return_object_on_allocation_failure()
+    { return {}; }
+
+    void * operator new(std::size_t const n) noexcept
+    { return std::malloc(n); }
+
+    void operator delete(void * const p) noexcept
+    { std::free(p); }
+};
+#endif
+
+template <std::movable T>
+struct Future {
+
+    T m_value{};
+
+    static constexpr bool await_ready() noexcept { return {}; }
+
+    template<typename coro>
+    constexpr auto await_suspend(coro const h) noexcept{
+        h.promise().m_finalValue = std::async([this,h]{
+            auto const end { m_value };
+            for (T i{1}; i < end; ++i) { m_value *= i; }
+            if (!h.done()) { h(); }
+            return m_value;
+        });
+    }
+
+    auto await_resume() const noexcept{ return m_value; }
+
+    //void await_transform() = delete;
+
+};
+
+// template <std::movable T>
+// auto operator co_await(Future<T> const & f) noexcept{
+//     std::cout << FUNC_SIGNATURE << " = " << f.m_value << std::endl;
+//     return f;
+// }
+
+static Generator<promise<std::future<int>>> f2() {
+    std::cout << FUNC_SIGNATURE << " begin" << std::endl;
+    Future f { 5} ;
+    auto const v { co_await f };
+    std::cout << FUNC_SIGNATURE << " Future::m_value = " << v << std::endl;
+    std::cout << FUNC_SIGNATURE << " end" << std::endl;
+}
+
+[[maybe_unused]] static void testF2()
 {
-    std::cout << "coroutine test begin" << std::endl;
+    std::cout << FUNC_SIGNATURE << " begin" << std::endl;
+    auto const ch{ f2() };
+    while (!ch.done()) {
+        ch.resume();
+        std::cout << "fuck" << std::endl;
+        std::cout << FUNC_SIGNATURE << " wait value = "
+            << ch.promiseRef()->m_finalValue.get()
+            << std::endl << std::flush;
+    }
 
+    std::cout << FUNC_SIGNATURE << " end" << std::endl;
+}
 
-    std::cout << "coroutine test end" << std::endl;
-
+int main() {
+    //testF1();
+    testF2();
     return 0;
 }
