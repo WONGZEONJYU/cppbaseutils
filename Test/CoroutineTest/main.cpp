290c5c20a578a7f284b015259d3cbccaf8c9ec73
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2026-01-05 15:16:58 +0800

    2026-01-05 update

--- Test/CoroutineTest/main.cpp
+++ Test/CoroutineTest/main.cpp
@@ -11,38 +11,33 @@ struct promise {
 
     constexpr promise(T && v) : m_value { v }{}
 
+    constexpr ~promise()
+    { std::cout << FUNC_SIGNATURE << std::endl; }
+
     T m_value{};
 
-    using coroutine_handle_type = XUtils::XCoroutineGenerator<promise>;
+    using XCoroutineGenerator = XUtils::XCoroutineGenerator<promise>;
+    using coroutine_handle = XCoroutineGenerator::coroutine_handle;
 
     auto get_return_object ()
-    { return coroutine_handle_type::from_promise(this); }
+    { return coroutine_handle::from_promise(*this); }
 
-    static std::suspend_always initial_suspend() noexcept
-    { return {}; }
+    static auto initial_suspend() noexcept
+    { return std::suspend_always{}; }
 
-    [[nodiscard]] auto final_suspend() const noexcept {
+    static auto final_suspend() noexcept
+    { return std::suspend_always {}; }
 
-        struct awaiter {
-            static constexpr bool await_ready() noexcept { return {}; }
-            static constexpr auto await_suspend(std::coroutine_handle<> const coro)  noexcept
-            { if (coro) { while (!coro.done()) { coro.resume(); } } }
-            static constexpr void await_resume() noexcept {}
-        };
-
-        return awaiter{};
-    }
-
-    void return_value(T v) noexcept
+    auto return_value(T v) noexcept
     { m_value = std::move(v); }
 
     auto yield_value(T v) noexcept
     { m_value = std::move(v); return std::suspend_always{};  }
 
-    void unhandled_exception() const noexcept { (void)this;std::terminate(); }
+    static auto unhandled_exception() noexcept { std::terminate(); }
 
-    static coroutine_handle_type get_return_object_on_allocation_failure()
-    { return {}; }
+    static auto get_return_object_on_allocation_failure()
+    { return coroutine_handle{}; }
 
     void * operator new(std::size_t const n) noexcept
     { return std::malloc(n); }
@@ -51,10 +46,11 @@ struct promise {
     { std::free(p); }
 };
 
-using promise1 = promise<int>;
-using XCoroutine1 = XUtils::XCoroutineGenerator<promise1>;
+struct Task1 : XUtils::XCoroutineGenerator<promise<int>> {
+    constexpr Task1(coroutine_handle const h) : XCoroutineGenerator { h } {}
+};
 
-[[maybe_unused]] static XCoroutine1 f1() {
+[[maybe_unused]] static Task1 f1() {
     std::cout << FUNC_SIGNATURE << " begin" << std::endl;
     for (int i{1};i <= 10; ++i) { co_yield i; }
     std::cout << FUNC_SIGNATURE << " end" << std::endl;
@@ -73,14 +69,15 @@ using XCoroutine1 = XUtils::XCoroutineGenerator<promise1>;
 #if 1
 
 template <std::movable T>
-struct Future {
+struct FutureAwait {
 
     T m_value{};
 
-    static constexpr auto await_ready() noexcept{ return false; }
+    static constexpr auto await_ready() noexcept
+    { return false; }
 
     template<typename coro>
-    constexpr auto await_suspend(coro const h) noexcept{
+    constexpr auto await_suspend(coro && h) noexcept{
         h.promise().m_finalValue = std::async([this,h]{
             auto const end { m_value };
             for (T i{1}; i < end; ++i) { m_value *= i; }
@@ -96,7 +93,8 @@ struct Future {
 template<std::movable T>
 struct promise< std::future<T> > {
 
-    using coroutine_handle = XUtils::XCoroutineGenerator<promise>::coroutine_handle;
+    using CoroutineGenerator = XUtils::XCoroutineGenerator<promise>;
+    using coroutine_handle = CoroutineGenerator::coroutine_handle;
     using future = std::future<T>;
     future m_finalValue {};
 
@@ -133,7 +131,7 @@ struct promise< std::future<T> > {
     { (void)this; std::terminate(); }
 
     static coroutine_handle get_return_object_on_allocation_failure()
-    { return {}; }
+    { return coroutine_handle{}; }
 
     void * operator new(std::size_t const n) noexcept {
         std::cout << FUNC_SIGNATURE << std::endl;
@@ -148,16 +146,18 @@ struct promise< std::future<T> > {
     template<typename U>
     static auto await_transform(U && u) {
         std::cout << FUNC_SIGNATURE << typeid(u).name() << std::endl;
-        return Future {u};
+        return FutureAwait {u};
     }
+};
+
+struct Task2 : XUtils::XCoroutineGenerator<promise<std::future<int>>> {
+    constexpr Task2( coroutine_handle const h ) noexcept
+        : XCoroutineGenerator { h } {  }
 };
 
 #endif
 
-using promise2 = promise<std::future<int>>;
-using Coroutine2 = XUtils::XCoroutineGenerator<promise2>;
-
-static Coroutine2 f2() {
+static Task2 f2() {
     std::cout << FUNC_SIGNATURE << " begin" << std::endl;
     auto const v { co_await 5 };
     std::cout << FUNC_SIGNATURE << " Future::m_value = " << v << std::endl;
@@ -167,6 +167,7 @@ static Coroutine2 f2() {
 [[maybe_unused]] static void testF2() {
     std::cout << FUNC_SIGNATURE << " begin" << std::endl;
     auto const ch{ f2() };
+    getchar();
     ch();
     ch.promise().m_finalValue.wait();
     std::cout << "wait value = "
