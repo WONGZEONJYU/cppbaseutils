95d861fbba7ccdc5e80279aa3604f605f5c76811
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2026-01-17 13:54:14 +0800

    2026-01-17 update

---
+++ Src/XQtHelper/qcoro/network/qcorolocalsocket.hpp
@@ -0,0 +1,24 @@
+#ifndef XUTILS2_Q_CORO_LOCAL_SOCKET_HPP
+#define XUTILS2_Q_CORO_LOCAL_SOCKET_HPP 1
+
+#include <XQtHelper/qcoro/core/qcoroiodevice.hpp>
+#include <QLocalSocket>
+#include <chrono>
+
+XTD_NAMESPACE_BEGIN
+XTD_INLINE_NAMESPACE_BEGIN(v1)
+
+namespace detail {
+
+
+
+
+
+
+
+}
+
+XTD_INLINE_NAMESPACE_END
+XTD_NAMESPACE_END
+
+#endif
