e730c389313bdd03710bf7145623a2640e498cd4
fuck_if <wongjy13005151320@outlook.com>
2026-07-07 01:32:42 +0800

    2026-07-07 update-1

--- Src/XCoroutine/xcoromanager.cpp
+++ Src/XCoroutine/xcoromanager.cpp
@@ -1,14 +1,44 @@
 #include <xcoromanager.hpp>
 
+XUtils::XCoroManager & XUtils::XCoroManager::instance() {
+    static XCoroManager mgr{};
+    return mgr;
+}
 
+std::size_t XUtils::XCoroManager::onlineSize() const noexcept {
+    std::shared_lock lk{ m_setMtx_ };
+    return m_handles_.size();
+}
 
+void XUtils::XCoroManager::isAllDone() const {
 
+    // auto const cb{ [this]()->std::function<void()> {
+    //     std::shared_lock setLk {m_setMtx_ },fnLk {m_fnMtx_ };
+    //     if (m_handles_.empty() && m_callback_)
+    //     { return m_callback_; }
+    //     return {};
+    // }() };
+    //
+    // if (cb) {
+    //     try { cb(); }
+    //     catch (std::exception const &) {}
+    //     setCallback(std::function<void()>{});
+    // }
+}
 
+XUtils::XCoroManager::XCoroManager() = default;
 
+bool XUtils::XCoroManager::addHandle(std::coroutine_handle<> const & h) const noexcept {
+    std::unique_lock lk{ m_setMtx_ };
+    auto const ok { m_handles_.insert(h).second };
+    if (ok) { m_online_.ref(); }
+    return ok;
+}
 
-
-
-
-
-
-
+void XUtils::XCoroManager::removeHandle(std::coroutine_handle<> const & h) const noexcept {
+    {
+        std::unique_lock lk{ m_setMtx_ };
+        if (m_handles_.erase(h) > 0) { m_online_.deref(); }
+    }
+    isAllDone();
+}
--- Src/XCoroutine/xcoromanager.hpp
+++ Src/XCoroutine/xcoromanager.hpp
@@ -2,25 +2,55 @@
 #ifndef XUTILS2_X_CORO_MANAGER_HPP_
 #define XUTILS2_X_CORO_MANAGER_HPP_ 1
 
+#include <algorithm>
 #include <XGlobal/xversion.hpp>
+#include <XGlobal/xclasshelpermacros.hpp>
+#include <XAtomic/xatomic.hpp>
+#include <unordered_set>
+#include <coroutine>
+#include <shared_mutex>
+#include <functional>
 
 XTD_NAMESPACE_BEGIN
-XTD_INLINE_NAMESPACE_BEGIN(v1)
+    XTD_INLINE_NAMESPACE_BEGIN(v1)
 
 namespace detail {
     template<typename T, template<typename> class TaskImpl, typename PromiseType>
     class XCoroTaskAbstract;
+
+    class TaskFinalSuspend;
 }
 
-class XCoroManager final {
+class X_CLASS_EXPORT XCoroManager final {
 
     template<typename T, template<typename> class TaskImpl, typename PromiseType>
     friend class detail::XCoroTaskAbstract;
 
+    friend class detail::TaskFinalSuspend;
+
+    mutable std::unordered_set<std::coroutine_handle<>> m_handles_{};
+    mutable std::function<void()> m_callback_{};
+    mutable std::shared_mutex m_setMtx_{},m_fnMtx_{};
+    mutable XAtomicInteger<std::size_t> m_online_{};
+
 public:
-    
+    X_DISABLE_COPY_MOVE(XCoroManager)
+
+    static XCoroManager & instance();
+    [[nodiscard]] std::size_t onlineSize() const noexcept;
 
 
+    template<typename Fn>
+    void setCallback(Fn && fn) const {
+        std::unique_lock lk{ m_fnMtx_ };
+        m_callback_ = std::forward<Fn>(fn);
+    }
+
+private:
+    XCoroManager();
+    void isAllDone() const;
+    bool addHandle(std::coroutine_handle<> const &) const noexcept;
+    void removeHandle(std::coroutine_handle<> const &) const noexcept;
 };
 
 XTD_INLINE_NAMESPACE_END
