b3ef4f92a05051dfe82bf900dc41439d56b80327
fuck_if <61109848+WONGZEONJYU@users.noreply.github.com>
2026-07-29 10:18:26 +0800

    2026-07-29 update-3

--- Test/CoroutineTest/main.cpp
+++ Test/CoroutineTest/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <XCoroutine/xcoroutinetask.hpp>
+#include <XCoroutine/xcorolazytask.hpp>
 #include <XHelper/xhelper.hpp>
-#include <XHelper/xtypetraits.hpp>
 
 template<typename T>
 struct Awaiter {
@@ -25,35 +24,45 @@ struct A {
     { return Awaiter<A>{}; }
 };
 
-XUtils::XCoroTask<> f1() {
+XUtils::XCoroTaskVoid f1() {
     std::cout << FUNC_SIGNATURE << " begin" << std::endl;
-    co_await A{};
     std::cout << FUNC_SIGNATURE << " end" << std::endl;
+    co_return;
 }
 
-XUtils::XCoroTask<> f2() {
+XUtils::XCoroTaskVoid f2() {
     std::cout << FUNC_SIGNATURE << " begin" << std::endl;
-    co_await f1();
     std::cout << FUNC_SIGNATURE << " end" << std::endl;
+    co_return;
 }
 
-template <typename T>
-void ff(T && t) {
-    std::cout << XUtils::typeName(std::forward<T>(t)) << std::endl;
+XUtils::XCoroTaskVoid f3() {
+    std::cout << FUNC_SIGNATURE << " begin" << std::endl;
+    std::cout << FUNC_SIGNATURE << " end" << std::endl;
+    co_return;
+}
+
+XUtils::XCoroTaskVoid fff()
+{
+    std::cout << FUNC_SIGNATURE << " begin" << std::endl;
+    //co_await (f1() | f2 | f3);
+    std::cout << FUNC_SIGNATURE << " end" << std::endl;
+    co_return;
 }
 
 int main() {
 
+#if 0
     XUtils::coroMgrRef().setAllExitCallback([] {
         std::cout << "COROEnd" << std::endl;
     });
-    //ff([]{ std::cout << FUNC_SIGNATURE << std::endl; });
-
     std::cout << XUtils::coroMgrRef().onlineSize() << std::endl;
-
     f2();
-
     std::cout << XUtils::coroMgrRef().onlineSize() << std::endl;
+#endif
+
+    fff() | f1 | f2 | f3;
+    fff() >> f1 >> f2 >> f3;
 
     return 0;
 }
